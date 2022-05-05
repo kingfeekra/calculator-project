10d746b91143b4a3359197f4503fe191eacdac80
kingfeekra <fiachraking@gmail.com>
2022-05-05 19:13:18 +0100

    Add number buttons 0-9
    Buttons 0-9, decimal point button "." and equals "=" are now
    visible on the webpage in a 4x3 grid. Buttons do not have
    functionality yet.

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+function operate(operator, a, b) {
+
+}
+
+const container = document.querySelector("#container");
+const buttonRows = [];
+const buttons = [];
+const numberOfButtons = 12;
+const buttonHeight = 40;
+const buttonWidth = 40;
+
+container.style.cssText = `height: ${buttonHeight*4}px; width: ${buttonWidth*3}px;`;
+
+for(let i = 0; i < numberOfButtons; i++) {
+    buttons[i] = document.createElement("button");
+    buttons[i].classList.add("button");
+    buttons[i].style.cssText = `height: ${buttonHeight}px; width: ${buttonWidth}px;`;
+    buttons[i].textContent = i + 1;
+
+    container.appendChild(buttons[i]);
+}
+buttons[9].textContent = ".";
+buttons[10].textContent = 0;
+buttons[11].textContent = "=";
+
+
+
+
