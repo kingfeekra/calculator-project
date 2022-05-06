d7dd21119944231a996ef855938a64471aa5b8e7
kingfeekra <fiachraking@gmail.com>
2022-05-06 21:05:01 +0100

    Previous number clears after operator is pressed
    If an operator button was the last button clicked, the display
    will clear when the next number button is clicked. The Previous
    displayed number is stored in an array for use later with the
    operator functions.
    
    comments added

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const buttonHeight = 40;
 const buttonWidth = 40;
 
 numberDiv.style.cssText = `height: ${buttonHeight*4}px; width: ${buttonWidth*3}px;`;
-
+//create number buttons 0-9, "." button and "=" button
 for(let i = 0; i < numberOfButtons; i++) {
     buttons[i] = document.createElement("button");
     buttons[i].classList.add("button");
@@ -49,11 +49,13 @@ buttons[9].textContent = ".";
 buttons[10].textContent = 0;
 buttons[11].textContent = "=";
 
+
 const operatorDiv = document.createElement("div");
 operatorDiv.classList.add("operatorDiv");
 buttonsDiv.appendChild(operatorDiv);
 const operatorButtons = [];
 
+//creates four operator buttons in separate div
 for(let i = 0; i < 4; i++) {
     operatorButtons[i] = document.createElement("button");
     operatorButtons[i].classList.add("operatorButtons");
@@ -64,17 +66,49 @@ operatorButtons[1].textContent = "-";
 operatorButtons[2].textContent = "x";
 operatorButtons[3].textContent = "÷";
 
+//creates clear button
 const clearButton = document.createElement("button");
 clearButton.classList.add("clearButton");
 clearButton.textContent = "C";
 buttonsDiv.appendChild(clearButton);
 
+let lastButtonClicked;
+
+//event listeners for number buttons, clear display if previous button click was operator
 const buttonPress = document.querySelectorAll(".button");
 for(let i = 0; i < buttonPress.length; i++) {
     buttonPress[i].addEventListener("click", () => {
-        displayDiv.textContent = buttons[i].textContent + displayDiv.textContent;
+        if(lastButtonClicked == "+") {
+            displayDiv.textContent = "";
+        }
+        lastButtonClicked = buttons[i].textContent;
+        displayDiv.textContent = displayDiv.textContent + buttons[i].textContent;
+        
+
+        
+        
+        console.log(lastButtonClicked);
     });
 }
+
+let sumArray = [];
+//event listeners for operator buttons, push number to array
+for(let i = 0; i < 4; i++) {
+    operatorButtons[i].addEventListener("click", () => {
+        lastButtonClicked = operatorButtons[i].textContent;
+        sumArray.push(displayDiv.textContent);
+        console.log(sumArray);
+        console.log(lastButtonClicked);
+        //displayDiv.textContent = operatorButtons[i].textContent;
+    })
+}
+
+
+
+//clear button listener to clear display
+clearButton.addEventListener("click", () => {
+    displayDiv.textContent = "";
+})
 
 
 
