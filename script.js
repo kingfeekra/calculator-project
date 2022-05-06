6dd115b2d655859e201a12e2f956128f51c45b3e
kingfeekra <fiachraking@gmail.com>
2022-05-06 15:16:27 +0100

    Add operator buttons & display box
    Operator buttons now displayed on the right side of the calculator
    Display box at top of calculator

--- script.js
+++ script.js
@@ -19,25 +19,48 @@ function operate(operator, a, b) {
 }
 
 const container = document.querySelector("#container");
+const displayDiv = document.createElement("div");
+const buttonsDiv = document.createElement("div");
+const numberDiv = document.createElement("div");
+container.appendChild(displayDiv);
+displayDiv.classList.add("displayDiv");
+container.appendChild(buttonsDiv);
+buttonsDiv.classList.add("buttonsDiv");
+buttonsDiv.appendChild(numberDiv);
+
 const buttonRows = [];
 const buttons = [];
 const numberOfButtons = 12;
 const buttonHeight = 40;
 const buttonWidth = 40;
 
-container.style.cssText = `height: ${buttonHeight*4}px; width: ${buttonWidth*3}px;`;
+numberDiv.style.cssText = `height: ${buttonHeight*4}px; width: ${buttonWidth*3}px;`;
 
 for(let i = 0; i < numberOfButtons; i++) {
     buttons[i] = document.createElement("button");
     buttons[i].classList.add("button");
     buttons[i].style.cssText = `height: ${buttonHeight}px; width: ${buttonWidth}px;`;
     buttons[i].textContent = i + 1;
-
-    container.appendChild(buttons[i]);
+    numberDiv.appendChild(buttons[i]);
 }
 buttons[9].textContent = ".";
 buttons[10].textContent = 0;
 buttons[11].textContent = "=";
+
+const operatorDiv = document.createElement("div");
+operatorDiv.classList.add("operatorDiv");
+buttonsDiv.appendChild(operatorDiv);
+const operatorButtons = [];
+
+for(let i = 0; i < 4; i++) {
+    operatorButtons[i] = document.createElement("button");
+    operatorButtons[i].classList.add("operatorButtons");
+    operatorDiv.appendChild(operatorButtons[i]);
+}
+operatorButtons[0].textContent = "+";
+operatorButtons[1].textContent = "-";
+operatorButtons[2].textContent = "x";
+operatorButtons[3].textContent = "÷";
 
 
 
